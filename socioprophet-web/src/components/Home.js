f4fbc12cd7fd7083449cf54844f62a45fe5fe2c5
Michael <mdheller@gmail.com>
2019-09-25 22:52:01 +1000

    test global header

--- socioprophet-web/src/components/Home.js
+++ socioprophet-web/src/components/Home.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import "./Home.css";
 import Top from "./Top";
 import TerminalDrawer from "./TerminalDrawer";
+import GlobalHeader from  "/.GlobalHeader";
 
 export default class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <div><Top /></div>
+        <div><GlobalHeader /></div>
         <div><TerminalDrawer /></div>
       </div>
     );
