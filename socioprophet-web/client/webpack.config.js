e7e5dd502837c6c2b3271be6cb61d03bc95ff876
Michael <mdheller@gmail.com>
2019-11-01 20:14:42 +1100

    8080 --> 8081

--- socioprophet-web/client/webpack.config.js
+++ socioprophet-web/client/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
   },
   devServer: {
     host: '0.0.0.0',
-    port: 8080,
+    port: 8081,
     historyApiFallback: true,
     allowedHosts: [
       'localhost.socioprophet.com',
