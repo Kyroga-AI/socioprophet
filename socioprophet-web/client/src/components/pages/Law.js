a1880b9c57119b30defae70566dd01c5154dd655
Michael <mdheller@gmail.com>
2019-11-02 14:08:08 +1100

    pages + styles

---
+++ socioprophet-web/client/src/components/pages/Law.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../components/layout/style-landing.css";
+import Landing from "../components/layout/Landing";
+
+class Landing extends Component { 
+  render() {
+    return ( 
+    )
+  }
+}
+
+export default Law;
