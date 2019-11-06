ff541c59bd86a162ebbdb142fa27ac8a2e1f50d7
Will Jones <willjones484@gmail.com>
2019-11-06 19:18:27 +1100

    changes

--- socioprophet-web/client/src/components/auth/Register.js
+++ socioprophet-web/client/src/components/auth/Register.js
@@ -5,7 +5,7 @@ import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
 import classnames from "classnames";
 import Button from "carbon-components-react/lib/components/Button";
-import "./RegisterStyles/styles.css";
+import "./RegisterStyles/register_styles.css";
 
 class Register extends Component {
   constructor() {
@@ -55,99 +55,141 @@ class Register extends Component {
     const { errors } = this.state;
 
     return (
-      <div className="root">
-        <div className="application">
-          <div className="masthead">
-            <div className="content-body">
-              <div className="modal-wrap">
-                <div className="modal-form">
-                  <div className="register">
-                    <div className="form-container">
-                      <h1 className="form-heading sp-h2">
-                        <span>Sign Up to SocioProphet</span>
-                      </h1>
-                      <div className="flex">
-                        <form className="row-form" noValidate onSubmit={this.onSubmit}>
-                          <div className="full-width">
-                            <p className="input-container">
-                              <label htmlFor="name">Name</label>
-                              <input
-                                onChange={this.onChange}
-                                value={this.state.name}
-                                error={errors.name}
-                                id="name"
-                                type="text"
-                                className={classnames("", {
-                                  invalid: errors.name
-                                })}
-                              />
-                              <span className="red-text">
-                                {errors.name}
-                              </span>
-                            </p>
-                            <p className="input-container">
-                              <label htmlFor="email">Email</label>
-                              <input
-                                onChange={this.onChange}
-                                value={this.state.email}
-                                error={errors.email}
-                                id="email"
-                                type="email"
-                                className={classnames("", {
-                                  invalid: errors.email
-                                })}
-                              />
-                              <span className="red-text">
-                                {errors.email}
-                              </span>
-                            </p>
-                            <p className="input-container">
-                              <label htmlFor="password">
-                                Password
-                              </label>
-                              <input
-                                onChange={this.onChange}
-                                value={this.state.password}
-                                error={errors.password}
-                                id="password"
-                                type="password"
-                                className={classnames("", {
-                                  invalid: errors.password
-                                })}
-                              />
-                              <span className="red-text">
-                                {errors.password}
-                              </span>
-                            </p>
-                            <p className="input-container">
-                              <label htmlFor="password2">
-                                Confirm Password
-                              </label>
-                              <input
-                                onChange={this.onChange}
-                                value={this.state.password2}
-                                error={errors.password2}
-                                id="password2"
-                                type="password"
-                                className={classnames("", {
-                                  invalid: errors.password2
-                                })}
-                              />
-                              <span className="red-text">
-                                {errors.password2}
-                              </span>
-                            </p>
-                          </div>
-                          <div>
-                            <Button type="submit">Sign Up</Button>
-                          </div>
-                        </form>
-                      </div>
+      <div className="page-wrapper_register">
+        <div className="contentNode_register">
+          <div className="landing-wrapper_register">
+            <nav className="landing-header_register">
+              <div className="landing-header-title_register">
+                <div className="landing-header-menuNavTitle_register">
+                  <a className="landing-header-titleText_register" href="/">
+                    <span className="landing-header-logoText_register"><strong>[ SocioProphet ] Platform</strong></span>
+                  </a>
+                </div>
+                <div className="landing-header-region_register"></div>
+              </div>
+              <nav className="landing-header-linksContainer_register">
+                <ul className="landing-header-links_register">
+                  <li className="landing-header-link_register"><a href="https://github.com/SocioProphet">GitHub</a></li>
+                  <li className="landing-header-link_register"><a href="https://medium.com/@socioprophet">Blog</a></li>
+                  <li className="landing-header-link_register"><a href="#">Docs</a></li>
+                  <div className="landing-header-separatorBlock_register">
+
+                    <Link to="/register" className="landing-header-signUpButton_register">Sign Up</Link>
+
+                  </div>
+                </ul>
+              </nav>
+            </nav>
+
+            <div className="main_register">
+              <div className="billboard-background_register">
+                <div className="billboard_register">
+                  <div className="main-socioprophet-name_register">
+                    Socio
+                    <span className="prophet_register">Prophet</span>
+                  </div>
+                  <div>
+                    <div className="main-platform-description_register">
+                      <strong>Open Collaborative Socio-Econo-Legislative Analytics.</strong>
                     </div>
                   </div>
                 </div>
               </div>
+              <div className="main-platform-section_register">
+
+                  <div className="register">
+
+                      <div>
+                      <form className="row-form" noValidate onSubmit={this.onSubmit}>
+                        <div className="full-width">
+                          <p className="input-container-left">
+                            <label htmlFor="name">Name</label>
+                            <input
+                              onChange={this.onChange}
+                              value={this.state.name}
+                              error={errors.name}
+                              id="name"
+                              type="text"
+                              className={classnames("", {
+                                invalid: errors.name
+                              })}
+                            />
+                            <span className="red-text">
+                              {errors.name}
+                            </span>
+                          </p>
+                          <p className="input-container-left">
+                            <label htmlFor="email">Email</label>
+                            <input
+                              onChange={this.onChange}
+                              value={this.state.email}
+                              error={errors.email}
+                              id="email"
+                              type="email"
+                              className={classnames("", {
+                                invalid: errors.email
+                              })}
+                            />
+                            <span className="red-text">
+                              {errors.email}
+                            </span>
+                          </p>
+                          <p className="input-container-right">
+                            <label htmlFor="password">
+                              Password
+                            </label>
+                            <input
+                              onChange={this.onChange}
+                              value={this.state.password}
+                              error={errors.password}
+                              id="password"
+                              type="password"
+                              className={classnames("", {
+                                invalid: errors.password
+                              })}
+                            />
+                            <span className="red-text">
+                              {errors.password}
+                            </span>
+                          </p>
+                          <p className="input-container-right">
+                            <label htmlFor="password2">
+                              Confirm Password
+                            </label>
+                            <input
+                              onChange={this.onChange}
+                              value={this.state.password2}
+                              error={errors.password2}
+                              id="password2"
+                              type="password"
+                              className={classnames("", {
+                                invalid: errors.password2
+                              })}
+                            />
+                            <span className="red-text">
+                              {errors.password2}
+                            </span>
+                          </p>
+                        </div>
+                        <div>
+                          <Button type="submit">Sign Up</Button>
+                        </div>
+                      </form>
+
+                      </div>
+                </div>
+
+              </div>
+
             </div>
+
+            <footer className="landing-footer_register">
+              <a className="landing-footer-link_register" href="#" target="_blank"><strong>Contact</strong></a>
+              <a className="landing-footer-link_register" href="#" target="_blank"><strong>Privacy</strong></a>
+              <a className="landing-footer-link_register" href="#" target="_blank"><strong>Terms of Use</strong></a>
+              <a className="landing-footer-link_register" href="#" target="_blank"><strong>Support</strong></a>
+            </footer>
+
           </div>
         </div>
       </div>
