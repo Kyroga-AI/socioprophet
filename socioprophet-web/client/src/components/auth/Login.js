93699da647dbb48e8a3b9aaf7582cb3df64a2ec1
Will Jones <willjones484@gmail.com>
2019-11-06 17:48:34 +1100

    login change

--- socioprophet-web/client/src/components/auth/Login.js
+++ socioprophet-web/client/src/components/auth/Login.js
@@ -1,96 +1,189 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import LoginModal from "./LoginModal";
-import "./style-landing.css";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { loginUser } from "../../actions/authActions";
+import classnames from "classnames";
+import Button from "carbon-components-react/lib/components/Button";
+import "./LoginStyles/login_styles.css";
 
-class Landing extends Component {
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
+  }
+
+  componentDidMount() {
+    // If logged in and user navigates to Login page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    this.props.loginUser(userData);
+  };
+
   render() {
+    const { errors } = this.state;
+
     return (
-      <div className="page-wrapper">
-        <div className="contentNode">
-          <div className="landing-wrapper">
-            <nav className="landing-header">
-              <div className="landing-header-title">
-                <div className="landing-header-menuNavTitle">
-                  <a className="landing-header-titleText" href="/">
-                    <span className="landing-header-logoText"><strong>[ SocioProphet ] Platform</strong></span>
+      <div className="page-wrapper_login">
+        <div className="contentNode_login">
+          <div className="landing-wrapper_login">
+            <nav className="landing-header_login">
+              <div className="landing-header-title_login">
+                <div className="landing-header-menuNavTitle_login">
+                  <a className="landing-header-titleText_login" href="/">
+                    <span className="landing-header-logoText_login"><strong>[ SocioProphet ] Platform</strong></span>
                   </a>
                 </div>
-                <div className="landing-header-region"></div>
+                <div className="landing-header-region_login"></div>
               </div>
-              <nav className="landing-header-linksContainer">
-                <ul className="landing-header-links">
-                  <li className="landing-header-link"><a href="https://github.com/SocioProphet">GitHub</a></li>
-                  <li className="landing-header-link"><a href="https://medium.com/@socioprophet">Blog</a></li>
-                  <li className="landing-header-link"><a href="#">Docs</a></li>
-                  <div className="landing-header-separatorBlock">
+              <nav className="landing-header-linksContainer_login">
+                <ul className="landing-header-links_login">
+                  <li className="landing-header-link_login"><a href="https://github.com/SocioProphet">GitHub</a></li>
+                  <li className="landing-header-link_login"><a href="https://medium.com/@socioprophet">Blog</a></li>
+                  <li className="landing-header-link_login"><a href="#">Docs</a></li>
+                  <div className="landing-header-separatorBlock_login">
 
-                    <Link to="/login" style={{ color : "#fff", fontSize : "16px" }}>Log In</Link>
-                    <Link to="/register" className="landing-header-signUpButton">Sign Up</Link>
+                    <Link to="/register" className="landing-header-signUpButton_login">Sign Up</Link>
 
                   </div>
                 </ul>
               </nav>
             </nav>
 
-            <div className="main">
-              <div className="billboard-background">
-                <div className="billboard">
-                  <div className="main-socioprophet-name">
+            <div className="main_login">
+              <div className="billboard-background_login">
+                <div className="billboard_login">
+                  <div className="main-socioprophet-name_login">
                     Socio
-                    <span className="prophet">Prophet</span>
+                    <span className="prophet_login">Prophet</span>
                   </div>
                   <div>
-                    <Link to="/login" className="main-button-login">Log In</Link>
-                    <Link to="/register" className="main-button-register">Try it for Free</Link>
+                    <div className="main-platform-description_login">
+                      <strong>Open Collaborative Socio-Econo-Legislative Analytics.</strong>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="main-platform-section">
-                <div className="main-platform-slogan">Explore the SocioProphet Platform</div>
-                <div className="main-platform-description">
-                  Open Collaborative Socio-Econo-Legislative Analytics.
+              <div className="main-platform-section_login">
+
+                  <div className="login">
+
+                      <div>
+                        <form className="row-form" noValidate onSubmit={this.onSubmit}>
+                          <div className="full-width">
+                            <p className="input-container">
+                              <label htmlFor="email">Email</label>
+                              <input
+                                onChange={this.onChange}
+                                value={this.state.email}
+                                error={errors.email}
+                                id="email"
+                                type="email"
+                                className={classnames("", {
+                                  invalid: errors.email || errors.emailnotfound
+                                })}
+                              />
+                              <span className="red-text">
+                                {errors.email}
+                                {errors.emailnotfound}
+                              </span>
+                            </p>
+                            <p className="input-container">
+                              <label htmlFor="password">
+                                Password
+
+                              </label>
+                              <input
+                                onChange={this.onChange}
+                                value={this.state.password}
+                                error={errors.password}
+                                id="password"
+                                type="password"
+                                className={classnames("", {
+                                  invalid: errors.password || errors.passwordincorrect
+                                })}
+                              />
+                              <span className="red-text">
+                                {errors.password}
+                                {errors.passwordincorrect}
+                              </span>
+                              <a className="fright" href="#">
+                                <span>Forgot Password?</span>
+                              </a>
+                            </p>
+                          </div>
+                          <div>
+                            <button className="continue-button_login" type="submit">Continue</button>
+                          </div>
+                        </form>
+
+                      </div>
                 </div>
+
               </div>
-              <div className="main-app-section">
-                <div className="offering-card-container">
-                  <LoginModal />
-                </div>
-                <div className="offering-card-container">
-                  <div className="offering-card-name">[ SocioProphet ] Data & AI</div>
-                  <div className="offering-card-description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Duis ut justo convallis.
-                  </div>
-                  <div className="offering-card-learnMore">
-                    <a href="#" target="_blank">Learn More</a>
-                  </div>
-                </div>
-                <div className="offering-card-container">
-                  <div className="offering-card-name">[ SocioProphet ] Society & Law</div>
-                  <div className="offering-card-description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Duis ut justo convallis.
-                  </div>
-                  <div className="offering-card-learnMore">
-                    <a href="#" target="_blank">Learn More</a>
-                  </div>
-                </div>
-              </div>
+
             </div>
 
-            <footer className="landing-footer">
-              <a className="landing-footer-link" href="#" target="_blank"><strong>Contact</strong></a>
-              <a className="landing-footer-link" href="#" target="_blank"><strong>Privacy</strong></a>
-              <a className="landing-footer-link" href="#" target="_blank"><strong>Terms of Use</strong></a>
-              <a className="landing-footer-link" href="#" target="_blank"><strong>Support</strong></a>
+            <footer className="landing-footer_login">
+              <a className="landing-footer-link_login" href="#" target="_blank"><strong>Contact</strong></a>
+              <a className="landing-footer-link_login" href="#" target="_blank"><strong>Privacy</strong></a>
+              <a className="landing-footer-link_login" href="#" target="_blank"><strong>Terms of Use</strong></a>
+              <a className="landing-footer-link_login" href="#" target="_blank"><strong>Support</strong></a>
             </footer>
 
           </div>
         </div>
       </div>
     );
+
+
+
   }
 }
 
-export default Landing;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { loginUser }
+)(Login);
--- socioprophet-web/client/src/components/auth/Register.js
+++ socioprophet-web/client/src/components/auth/Register.js
@@ -1,91 +1,153 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import RegisterModal from "./RegisterModal";
-import "./style-landing.css";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/authActions";
+import classnames from "classnames";
+import Button from "carbon-components-react/lib/components/Button";
+import "./RegisterStyles/styles.css";
 
-class Landing extends Component {
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      password2: "",
+      errors: {}
+    };
+  }
+
+  componentDidMount() {
+    // If logged in and user navigates to Register page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+
+    this.props.registerUser(newUser, this.props.history);
+  };
+
   render() {
+    const { errors } = this.state;
+
     return (
-      <div className="page-wrapper">
-        <div className="contentNode">
-          <div className="landing-wrapper">
-            <nav className="landing-header">
-              <div className="landing-header-title">
-                <div className="landing-header-menuNavTitle">
-                  <a className="landing-header-titleText" href="/">
-                    <span className="landing-header-logoText"><strong>[ SocioProphet ] Platform</strong></span>
-                  </a>
-                </div>
-                <div className="landing-header-region"></div>
-              </div>
-              <nav className="landing-header-linksContainer">
-                <ul className="landing-header-links">
-                  <li className="landing-header-link"><a href="https://github.com/SocioProphet">GitHub</a></li>
-                  <li className="landing-header-link"><a href="https://medium.com/@socioprophet">Blog</a></li>
-                  <li className="landing-header-link"><a href="#">Docs</a></li>
-                  <div className="landing-header-separatorBlock">
-
-                    <Link to="/login" style={{ color : "#fff", fontSize : "16px" }}>Log In</Link>
-                    <Link to="/register" className="landing-header-signUpButton">Sign Up</Link>
-
-                  </div>
-                </ul>
-              </nav>
-            </nav>
-
-            <div className="main">
-              <div className="billboard-background">
-                <div className="billboard">
-                  <div className="main-socioprophet-name">
-                    Socio
-                    <span className="prophet">Prophet</span>
-                  </div>
-                  <div>
-                    <Link to="/login" className="main-button-login">Log In</Link>
-                    <Link to="/register" className="main-button-register">Try it for Free</Link>
-                  </div>
-                </div>
-              </div>
-              <div className="main-platform-section">
-                <div className="main-platform-slogan">Explore the SocioProphet Platform</div>
-                <div className="main-platform-description">
-                  Open Collaborative Socio-Econo-Legislative Analytics.
-                </div>
-              </div>
-              <div className="main-app-section">
-                <div className="offering-card-container">
-                  <RegisterModal />
-                </div>
-                <div className="offering-card-container">
-                  <div className="offering-card-name">[ SocioProphet ] Data & AI</div>
-                  <div className="offering-card-description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Duis ut justo convallis.
-                  </div>
-                  <div className="offering-card-learnMore">
-                    <a href="#" target="_blank">Learn More</a>
-                  </div>
-                </div>
-                <div className="offering-card-container">
-                  <div className="offering-card-name">[ SocioProphet ] Society & Law</div>
-                  <div className="offering-card-description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Duis ut justo convallis.
-                  </div>
-                  <div className="offering-card-learnMore">
-                    <a href="#" target="_blank">Learn More</a>
+      <div className="root">
+        <div className="application">
+          <div className="masthead">
+            <div className="content-body">
+              <div className="modal-wrap">
+                <div className="modal-form">
+                  <div className="register">
+                    <div className="form-container">
+                      <h1 className="form-heading sp-h2">
+                        <span>Sign Up to SocioProphet</span>
+                      </h1>
+                      <div className="flex">
+                        <form className="row-form" noValidate onSubmit={this.onSubmit}>
+                          <div className="full-width">
+                            <p className="input-container">
+                              <label htmlFor="name">Name</label>
+                              <input
+                                onChange={this.onChange}
+                                value={this.state.name}
+                                error={errors.name}
+                                id="name"
+                                type="text"
+                                className={classnames("", {
+                                  invalid: errors.name
+                                })}
+                              />
+                              <span className="red-text">
+                                {errors.name}
+                              </span>
+                            </p>
+                            <p className="input-container">
+                              <label htmlFor="email">Email</label>
+                              <input
+                                onChange={this.onChange}
+                                value={this.state.email}
+                                error={errors.email}
+                                id="email"
+                                type="email"
+                                className={classnames("", {
+                                  invalid: errors.email
+                                })}
+                              />
+                              <span className="red-text">
+                                {errors.email}
+                              </span>
+                            </p>
+                            <p className="input-container">
+                              <label htmlFor="password">
+                                Password
+                              </label>
+                              <input
+                                onChange={this.onChange}
+                                value={this.state.password}
+                                error={errors.password}
+                                id="password"
+                                type="password"
+                                className={classnames("", {
+                                  invalid: errors.password
+                                })}
+                              />
+                              <span className="red-text">
+                                {errors.password}
+                              </span>
+                            </p>
+                            <p className="input-container">
+                              <label htmlFor="password2">
+                                Confirm Password
+                              </label>
+                              <input
+                                onChange={this.onChange}
+                                value={this.state.password2}
+                                error={errors.password2}
+                                id="password2"
+                                type="password"
+                                className={classnames("", {
+                                  invalid: errors.password2
+                                })}
+                              />
+                              <span className="red-text">
+                                {errors.password2}
+                              </span>
+                            </p>
+                          </div>
+                          <div>
+                            <Button type="submit">Sign Up</Button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-
-            <footer className="landing-footer">
-              <a className="landing-footer-link" href="#" target="_blank"><strong>Contact</strong></a>
-              <a className="landing-footer-link" href="#" target="_blank"><strong>Privacy</strong></a>
-              <a className="landing-footer-link" href="#" target="_blank"><strong>Terms of Use</strong></a>
-              <a className="landing-footer-link" href="#" target="_blank"><strong>Support</strong></a>
-            </footer>
-
           </div>
         </div>
       </div>
@@ -93,4 +155,18 @@ class Landing extends Component {
   }
 }
 
-export default Landing;
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { registerUser }
+)(withRouter(Register));
