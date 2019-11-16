0099b0385a5cb8ab5f36aa585e36a2724fbc1cef
Michael <mdheller@gmail.com>
2019-11-16 12:49:26 +1100

    users.js fix

--- socioprophet-web/routes/api/users.js
+++ socioprophet-web/routes/api/users.js
@@ -6,11 +6,11 @@ const keys = require("../../src/validation/config/keys");
 const passport = require("passport");
 
 // Load input validation
-const validateRegisterInput = require("../../validation/register");
-const validateLoginInput = require("../../validation/login");
+const validateRegisterInput = require("../../src/config/validation/register");
+const validateLoginInput = require("../../src/config/validation/login");
 
 // Load User model
-const User = require("../../models/User");
+const User = require("../../src/models/User");
 
 // @route POST api/users/register
 // @desc Register user
