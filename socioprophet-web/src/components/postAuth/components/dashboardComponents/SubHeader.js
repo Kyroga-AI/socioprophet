dda33655c803e349d0af6a9360e5d166983a06e8
Will Jones <willjones484@gmail.com>
2020-02-08 11:41:28 +1100

    sub

--- socioprophet-web/src/components/postAuth/components/dashboardComponents/SubHeader.js
+++ socioprophet-web/src/components/postAuth/components/dashboardComponents/SubHeader.js
@@ -10,21 +10,20 @@ class SubHeader extends Component {
     return (
       <div className="sub">
         <div className="sub--ticker">
-
-        <Ticker>
-          {({ index }) => (
-            <div className="ticker">
-              <p><span className="news">News</span> / IM's #{index}!</p>
-            </div>
-          )}
-        </Ticker>
-        <div className="sub--menu">
-          <Breadcrumb>
-            <BreadcrumbItem href="#">Breadcrumb 1</BreadcrumbItem>
-            <BreadcrumbItem href="#">Breadcrumb 2</BreadcrumbItem>
-            <BreadcrumbItem href="#">Breadcrumb 3</BreadcrumbItem>
-          </Breadcrumb>
-        </div>
+          <Ticker>
+            {({ index }) => (
+              <div className="ticker">
+                <p><span className="news">News</span> / IM's #{index}!</p>
+              </div>
+            )}
+          </Ticker>
+          <div className="sub--menu">
+            <Breadcrumb>
+              <BreadcrumbItem href="#">Breadcrumb 1</BreadcrumbItem>
+              <BreadcrumbItem href="#">Breadcrumb 2</BreadcrumbItem>
+              <BreadcrumbItem href="#">Breadcrumb 3</BreadcrumbItem>
+            </Breadcrumb>
+          </div>
         </div>
       </div>
     );
