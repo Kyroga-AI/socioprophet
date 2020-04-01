b99f772ad201939370bd765894ca784db77898f8
Will Jones <willjones484@gmail.com>
2020-04-01 21:25:34 +1100

    Complete Project Page Templates (1, 2, 3)

--- socioprophet-web/src/components/postAuth/components/dashboardComponents/pages/Projects1.js
+++ socioprophet-web/src/components/postAuth/components/dashboardComponents/pages/Projects1.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Form from "carbon-components-react/lib/components/Form";
-import FormGroup from "carbon-components-react/lib/components/FormGroup";
 import FormLabel from "carbon-components-react/lib/components/FormLabel";
 import TextArea from "carbon-components-react/lib/components/TextArea";
 import TextInput from "carbon-components-react/lib/components/TextInput";
@@ -13,96 +12,94 @@ import "./styles/projects1.css";
 class Projects1 extends Component {
   render() {
     return (
-      <div className="project__sOne">
-          <div className="project__sOne__new">
-            <div className="project__sOne__new__header">
-              <h3 className="project__sOne__new__header__title">New Project</h3>
-            </div>
-              <Form
-                className="project__sOne__new__form"
-                onSubmit={function noRefCheck(){}}
-              >
-                <h4 className="project__sOne__new__form__heading">Define project details</h4>
-                <div style={{ marginBottom : "2rem" }}>
-                  <TextInput
-                    disabled={false}
-                    light={true}
-                    id="projectName"
-                    labelText="Name"
-                    placeholder="Project name"
-                    type="text"
-                  />
-                </div>
-                <div>
-                  <TextArea
-                    disabled={false}
-                    light={true}
-                    cols={100}
-                    id="projectName"
-                    labelText="Description"
-                    placeholder="Project description"
-                    type="text"
-                  />
-                </div>
-                <h4 className="project__sOne__new__form__heading"><span style={{ paddingTop : "2rem" }}>Collaboration model</span></h4>
-                <div className="sOnecontentForm__radio">
-                  <RadioButtonGroup
-                    defaultSelected="default-selected"
-                    labelPosition="right"
-                    name="radio-button-group--privacy"
-                    onChange={function noRefCheck(){}}
-                    orientation="horizontal"
-                  >
-                    <RadioButton
-                      id="radio-1"
-                      labelText="Public"
-                      value="default-selected"
-                    />
-                    <RadioButton
-                      id="radio-2"
-                      labelText="Private"
-                      value="standard"
-                    />
-                  </RadioButtonGroup>
-                  <RadioButtonGroup
-                    defaultSelected="default-selected"
-                    labelPosition="right"
-                    name="radio-button-group--searchable"
-                    onChange={function noRefCheck(){}}
-                    orientation="horizontal"
-                  >
-                    <RadioButton
-                      id="radio-3"
-                      labelText="Open"
-                      value="default-selected"
-                    />
-                    <RadioButton
-                      id="radio-4"
-                      labelText="Closed"
-                      value="standard"
-                    />
-                  </RadioButtonGroup>
-                </div>
-              </Form>
-              <div className="sOne__footer">
-                <div className="sOnecontentForm__pageBtn">
-                  <Button
-                    className="cancel"
-                    size="small"
-                  >
-                    <Link className="sOnecancel__link" to="/dashboard">Cancel</Link>
-                  </Button>
-                  <Button
-                    className="next"
-                    size="small"
-                  >
-                    <Link className="sOnenext__link" to="/projects2">Next</Link>
-                  </Button>
-                </div>
-              </div>
-
+      <div className="sOne">
+        <div className="new">
+          <div className="new__header">
+            <h3 className="new__header__title">New Project</h3>
           </div>
-
+          <Form
+            className="new__form"
+            onSubmit={function noRefCheck(){}}
+          >
+            <h4 className="new__form__heading">Define project details</h4>
+            <div style={{ marginBottom : "2rem" }}>
+              <TextInput
+                disabled={false}
+                light={true}
+                id="projectName"
+                labelText="Name"
+                placeholder="Project name"
+                type="text"
+              />
+            </div>
+            <div>
+              <TextArea
+                disabled={false}
+                light={true}
+                cols={100}
+                id="projectName"
+                labelText="Description"
+                placeholder="Project description"
+                type="text"
+              />
+            </div>
+            <h4 className="new__form__heading"><span style={{ paddingTop : "2rem" }}>Collaboration model</span></h4>
+            <div className="new__form__collab">
+              <RadioButtonGroup
+                defaultSelected="default-selected"
+                labelPosition="right"
+                name="radio-button-group--privacy"
+                onChange={function noRefCheck(){}}
+                orientation="horizontal"
+              >
+                <RadioButton
+                  id="radio-1"
+                  labelText="Public"
+                  value="default-selected"
+                />
+                <RadioButton
+                  id="radio-2"
+                  labelText="Private"
+                  value="standard"
+                />
+              </RadioButtonGroup>
+              <RadioButtonGroup
+                defaultSelected="default-selected"
+                labelPosition="right"
+                name="radio-button-group--searchable"
+                onChange={function noRefCheck(){}}
+                orientation="horizontal"
+              >
+                <RadioButton
+                  id="radio-3"
+                  labelText="Open"
+                  value="default-selected"
+                />
+                <RadioButton
+                  id="radio-4"
+                  labelText="Closed"
+                  value="standard"
+                />
+              </RadioButtonGroup>
+            </div>
+          </Form>
+          <div className="new__footer">
+            <div className="new__footer__pageBtn">
+              <Button
+                className="cancel"
+                size="small"
+              >
+                <Link className="new__footer__pageBtn__cancelLink" to="/dashboard">Cancel</Link>
+              </Button>
+              <Button
+                className="next"
+                size="small"
+              >
+                <Link className="new__footer__pageBtn__nextLink" to="/projects2">Next</Link>
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- socioprophet-web/src/components/postAuth/components/dashboardComponents/pages/Projects2.js
+++ socioprophet-web/src/components/postAuth/components/dashboardComponents/pages/Projects2.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
 import Form from "carbon-components-react/lib/components/Form";
-import FormGroup from "carbon-components-react/lib/components/FormGroup";
 import FormLabel from "carbon-components-react/lib/components/FormLabel";
-import TextArea from "carbon-components-react/lib/components/TextArea";
-import TextInput from "carbon-components-react/lib/components/TextInput";
-import RadioButtonGroup from "carbon-components-react/lib/components/RadioButtonGroup";
-import RadioButton from "carbon-components-react/lib/components/RadioButton";
+import Checkbox from "carbon-components-react/lib/components/Checkbox";
+import NumberInput from "carbon-components-react/lib/components/NumberInput";
 import Button from "carbon-components-react/lib/components/Button";
 import { Link } from "react-router-dom";
 
@@ -13,42 +10,74 @@ import "./styles/projects2.css";
 class Projects2 extends Component {
   render() {
     return (
-      <div className="dashboard__projects">
-        <div>
-          <div className="dashboard__projects__newProject">
-            <div className="dashboard__projects__newProject__header">
-              <h3 className="headerTitle">Compute & Storage</h3>
+      <div className="sTwo">
+        <div className="new">
+          <div className="new__header">
+            <h3 className="new__header__title">Compute & Storage</h3>
+          </div>
+          <Form
+            className="new__form"
+            onSubmit={function noRefCheck(){}}
+          >
+            <h4 className="new__form__heading">Compute Options</h4>
+            <div style={{ marginBottom : "2rem" }}>
+              <Checkbox
+                className="some-class"
+                defaultChecked
+                id="checkbox-0"
+                indeterminate={false}
+                labelText="Compute option 1"
+                onChange={function noRefCheck(){}}
+              />
+              <Checkbox
+                className="some-class"
+                id="checkbox-1"
+                indeterminate={false}
+                labelText="Compute option 2"
+                onChange={function noRefCheck(){}}
+              />
+              <Checkbox
+                className="some-class"
+                id="checkbox-2"
+                indeterminate={false}
+                labelText="Compute option 3"
+                onChange={function noRefCheck(){}}
+              />
+              <Checkbox
+                className="some-class"
+                disabled
+                id="checkbox-3"
+                indeterminate={false}
+                labelText="Extend compute"
+                onChange={function noRefCheck(){}}
+              />
             </div>
-            <div className="dashboard__projects__newProject__content">
-              <Form
-                className="contentForm"
-                onSubmit={function noRefCheck(){}}
+            <h4 className="new__form__heading"><span style={{paddingTop:"2rem"}}>Collaborator Restrictions</span></h4>
+            <NumberInput
+              className="some-class"
+              id="number-input-1"
+              label="Collaborators"
+              max={100}
+              min={0}
+              step={1}
+              value={1}
+            />
+          </Form>
+          <div className="new__footer">
+            <div className="new__footer__pageBtn">
+              <Button
+                className="cancel"
+                size="small"
               >
-                <h4 className="contentForm__heading">Compute Options</h4>
-
-                <h4 className="contentForm__heading"><span style={{paddingTop:"2rem"}}>Collaborator Restrictions</span></h4>
-
-                <div className="contentForm__pageBtn">
-                  <Button
-                    className="cancel"
-                    size="small"
-                  >
-                    <Link className="cancel__link" to="/dashboard">Cancel</Link>
-                  </Button>
-                  <Button
-                    className="next"
-                    size="small"
-                  >
-                    <Link className="next__link" to="/projects3">Next</Link>
-                  </Button>
-                </div>
-              </Form>
+                <Link className="new__footer__pageBtn__cancelLink" to="/dashboard">Cancel</Link>
+              </Button>
+              <Button
+                className="next"
+                size="small"
+              >
+                <Link className="new__footer__pageBtn__nextLink" to="/projects3">Next</Link>
+              </Button>
             </div>
-
-            <div className="dashboard__projects__form__footer">
-
-            </div>
-
           </div>
         </div>
       </div>
