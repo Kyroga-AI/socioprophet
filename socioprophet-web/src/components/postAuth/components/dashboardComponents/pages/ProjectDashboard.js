069d3022ee279eb1ecaac3ac562986b5c504955f
Will Jones <willjones484@gmail.com>
2020-05-22 16:45:11 +1000

    add project dashboard page

---
+++ socioprophet-web/src/components/postAuth/components/dashboardComponents/pages/ProjectDashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ProjectDashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Project Dashboard</h1>
+      </div>
+    );
+  }
+}
+
+export default ProjectDashboard;
