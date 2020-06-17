2f1433079811eb8b20544b24055d36c20497e9ec
Will Jones <willjones484@gmail.com>
2020-06-17 13:16:54 +1000

    project styles

--- socioprophet-web/src/components/postAuth/interface_components/dashboard_components/content/ProjectDashboard.js
+++ socioprophet-web/src/components/postAuth/interface_components/dashboard_components/content/ProjectDashboard.js
@@ -7,78 +7,80 @@ class ProjectDashboard extends Component {
     return (
       <div className="project--home">
         <h1 className="project--home__heading">Project List</h1>
-        <DataTable
-          filterRows={undefined}
-          headers={[
-            {
-              header: "Latest Configuration",
-              key: "dateConfigured",
-            },
-            {
-              header: "Project Name",
-              key: "projectName",
-            },
+        <div className="project--home__dataTable">
+          <DataTable
+            filterRows={undefined}
+            headers={[
+              {
+                header: "Latest Configuration",
+                key: "dateConfigured",
+              },
+              {
+                header: "Project Name",
+                key: "projectName",
+              },
 
-            {
-              header: "Public",
-              key: "publicPrivate",
-            },
-            {
-              header: "Open",
-              key: "openClosed",
-            },
-            {
-              header: "Compute",
-              key: "computeOptions",
-            },
-            {
-              header: "Cluster",
-              key: "clusterOptions",
-            },
-            {
-              header: "Collaborator Restrictions",
-              key: "collaborators",
-            },
-          ]}
-          isSelectable={false}
-          isSortable={false}
-          missingDataCharacter="–"
-          render={undefined}
-          rows={[
-            {
-              id: "1",
-              dateConfigured: "4 June 20",
-              projectName: "SP Project",
-              publicPrivate: "true",
-              openClosed: "false",
-              computeOptions: "option 1",
-              clusterOptions: "cluster 3",
-              collaborators: "3",
-            },
-            {
-              id: "2",
-              dateConfigured: "5 June 20",
-              projectName: "my socioprophet test project",
-              publicPrivate: "false",
-              openClosed: "false",
-              computeOptions: "option 2",
-              clusterOptions: "cluster 1",
-              collaborators: "1",
-            },
-            {
-              id: "3",
-              dateConfigured: "6 June 20",
-              projectName: "test-sp",
-              publicPrivate: "true",
-              openClosed: "true",
-              computeOptions: "option 3",
-              clusterOptions: "cluster 2",
-              collaborators: "100",
-            },
-          ]}
-          sortRow={undefined}
-          useZebraStyles={false}
-        />
+              {
+                header: "Public",
+                key: "publicPrivate",
+              },
+              {
+                header: "Open",
+                key: "openClosed",
+              },
+              {
+                header: "Compute",
+                key: "computeOptions",
+              },
+              {
+                header: "Cluster",
+                key: "clusterOptions",
+              },
+              {
+                header: "Collaborator Restrictions",
+                key: "collaborators",
+              },
+            ]}
+            isSelectable={false}
+            isSortable={false}
+            missingDataCharacter="–"
+            render={undefined}
+            rows={[
+              {
+                id: "1",
+                dateConfigured: "4 June 20",
+                projectName: "SP Project",
+                publicPrivate: "true",
+                openClosed: "false",
+                computeOptions: "option 1",
+                clusterOptions: "cluster 3",
+                collaborators: "3",
+              },
+              {
+                id: "2",
+                dateConfigured: "5 June 20",
+                projectName: "my socioprophet test project",
+                publicPrivate: "false",
+                openClosed: "false",
+                computeOptions: "option 2",
+                clusterOptions: "cluster 1",
+                collaborators: "1",
+              },
+              {
+                id: "3",
+                dateConfigured: "6 June 20",
+                projectName: "test-sp",
+                publicPrivate: "true",
+                openClosed: "true",
+                computeOptions: "option 3",
+                clusterOptions: "cluster 2",
+                collaborators: "100",
+              },
+            ]}
+            sortRow={undefined}
+            useZebraStyles={false}
+          />
+        </div>
       </div>
     );
   }
