9facd04280f79695554423828a97b7d117a6f67b
Will Jones <willjones484@gmail.com>
2020-06-20 17:32:00 +1000

    fix global bar on register page

--- socioprophet-web/src/components/landing/Register.js
+++ socioprophet-web/src/components/landing/Register.js
@@ -83,6 +83,23 @@ class Register extends Component {
             render={({ isSideNavExpanded, onClickSideNavExpand }) => (
               <Header aria-label="SocioProphet Platform">
                 <HeaderGlobalBar>
+                  {/* <Link to="/" className="header__register__back">
+                    &#945; Registry
+                  </Link> */}
+                  <HeaderGlobalAction aria-label="App Switcher">
+                    <svg
+                      focusable="false"
+                      preserveAspectRatio="xMidYMid meet"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      aria-hidden="true"
+                    >
+                      <path d="M18 18h3v3h-3zm-7.5 0h3v3h-3zM3 18h3v3H3zm15-7.5h3v3h-3zm-7.5 0h3v3h-3zm-7.5 0h3v3H3zM18 3h3v3h-3zm-7.5 0h3v3h-3zM3 3h3v3H3z"></path>
+                    </svg>
+                  </HeaderGlobalAction>
+
                   <HeaderGlobalAction
                     aria-label="App Switcher"
                     isActive={isSideNavExpanded}
