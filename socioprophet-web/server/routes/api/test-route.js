fbcfc59895f2f986834a483cb1311b9984f5e9bc
Will Jones <willjones484@gmail.com>
2021-06-24 13:01:52 +1000

    d

--- socioprophet-web/server/routes/api/test-route.js
+++ socioprophet-web/server/routes/api/test-route.js
@@ -12,59 +12,59 @@ const url = "https://hnrss.org/newest";
 // change this to dotenv
 // const serviceAccount = require("./adminSDKToken.json");
 
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-});
+// admin.initializeApp({
+//   credential: admin.credential.applicationDefault(),
+//   databaseURL: process.env.FIREBASE_DATABASE_URL,
+// });
 
 router.post("/data", (req, res) => {
   const email = req.body.email;
-  const actionCodeSettings = {
-    url: "https://socioprophet.com/signup",
-    handleCodeInApp: true,
-  };
-  admin
-    .auth()
-    .generateSignInWithEmailLink(email, actionCodeSettings)
-    .then((link) => {
-      let transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: process.env.GMAIL_USERNAME,
-          pass: process.env.GMAIL_PASSWORD,
-          clientId: process.env.GMAIL_CLIENT_ID,
-          clientSecret: process.env.GMAIL_CLIENT_SECRET,
-          refreshToken: process.env.GMAIL_REFRESH_TOKEN,
-        },
-      });
-      let mailOptions = {
-        from: "noreply@socioprophet.ai",
-        to: email,
-        subject: "SignIn Requested",
-        html: `
-          <h1>SocioProphet&#174;</h1>
-          <p>Hello,</p>
-          <p>We received a request from this email to sign in to SocioProphet.</p>
-          <p>If you would like to sign in via ${email}, you can follow this link:</p>
-          <a href=${link}>Sign in to SocioProphet!</a>
-          <br />
-          <p>If you did not request this link to sign in to SocioProphet, you can safely ignore this email.</p>
-          <br />
-          <p>Thanks!</p>
-          <p>The SocioProphet Team</p>
-        `,
-      };
-      transporter.sendMail(mailOptions, function (err, data) {
-        if (err) {
-          console.log("Error " + err);
-        } else {
-          console.log("Email sent successfully");
-        }
-      });
-    })
-    .catch((err) => {
-      console.error("An error happened: " + err);
-    });
+  // const actionCodeSettings = {
+  //   url: "https://socioprophet.com/signup",
+  //   handleCodeInApp: true,
+  // };
+  // admin
+  //   .auth()
+  //   .generateSignInWithEmailLink(email, actionCodeSettings)
+  //   .then((link) => {
+  //     let transporter = nodemailer.createTransport({
+  //       service: "gmail",
+  //       auth: {
+  //         user: process.env.GMAIL_USERNAME,
+  //         pass: process.env.GMAIL_PASSWORD,
+  //         clientId: process.env.GMAIL_CLIENT_ID,
+  //         clientSecret: process.env.GMAIL_CLIENT_SECRET,
+  //         refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+  //       },
+  //     });
+  //     let mailOptions = {
+  //       from: "noreply@socioprophet.ai",
+  //       to: email,
+  //       subject: "SignIn Requested",
+  //       html: `
+  //         <h1>SocioProphet&#174;</h1>
+  //         <p>Hello,</p>
+  //         <p>We received a request from this email to sign in to SocioProphet.</p>
+  //         <p>If you would like to sign in via ${email}, you can follow this link:</p>
+  //         <a href=${link}>Sign in to SocioProphet!</a>
+  //         <br />
+  //         <p>If you did not request this link to sign in to SocioProphet, you can safely ignore this email.</p>
+  //         <br />
+  //         <p>Thanks!</p>
+  //         <p>The SocioProphet Team</p>
+  //       `,
+  //     };
+  //     transporter.sendMail(mailOptions, function (err, data) {
+  //       if (err) {
+  //         console.log("Error " + err);
+  //       } else {
+  //         console.log("Email sent successfully");
+  //       }
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     console.error("An error happened: " + err);
+  //   });
   // res.json({
   //   "gmail-username": process.env.GMAIL_USERNAME,
   //   "gmail-password": process.env.GMAIL_PASSWORD,
@@ -73,6 +73,41 @@ router.post("/data", (req, res) => {
   //   "gmail-refresh-token": process.env.GMAIL_REFRESH_TOKEN,
   // });
   // res.send("gg");
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GMAIL_USERNAME,
+      pass: process.env.GMAIL_PASSWORD,
+      clientId: process.env.GMAIL_CLIENT_ID,
+      clientSecret: process.env.GMAIL_CLIENT_SECRET,
+      refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+    },
+  });
+  let mailOptions = {
+    from: "noreply@socioprophet.ai",
+    to: email,
+    subject: "SignIn Requested",
+    html: `
+          <h1>SocioProphet&#174;</h1>
+          <p>Hello,</p>
+          <p>We received a request from this email to sign in to SocioProphet.</p>
+          <p>If you would like to sign in via ${email}, you can follow this link:</p>
+          <a href=>Sign in to SocioProphet!</a>
+          <br />
+          <p>If you did not request this link to sign in to SocioProphet, you can safely ignore this email.</p>
+          <br />
+          <p>Thanks!</p>
+          <p>The SocioProphet Team</p>
+        `,
+  };
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log("Error " + err);
+    } else {
+      console.log("Email sent successfully");
+    }
+  });
+
   res.json({ requestBody: "data" });
 });
 
