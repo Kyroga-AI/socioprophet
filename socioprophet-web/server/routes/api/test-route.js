6f53d2e978730271b2a5bbed8a8b2eea0337e833
Will Jones <willjones484@gmail.com>
2021-06-24 11:43:03 +1000

    server image

--- socioprophet-web/server/routes/api/test-route.js
+++ socioprophet-web/server/routes/api/test-route.js
@@ -17,61 +17,62 @@ admin.initializeApp({
   databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
 
-router.get("/data", (req, res) => {
-  // const email = req.body.email;
-  // const actionCodeSettings = {
-  //   url: "http://localhost:8081/signup",
-  //   handleCodeInApp: true,
-  // };
-  // admin
-  //   .auth()
-  //   .generateSignInWithEmailLink(email, actionCodeSettings)
-  //   .then((link) => {
-  //     let transporter = nodemailer.createTransport({
-  //       service: "gmail",
-  //       auth: {
-  //         user: process.env.GMAIL_USERNAME,
-  //         pass: process.env.GMAIL_PASSWORD,
-  //         clientId: process.env.GMAIL_CLIENT_ID,
-  //         clientSecret: process.env.GMAIL_CLIENT_SECRET,
-  //         refreshToken: process.env.GMAIL_REFRESH_TOKEN,
-  //       },
-  //     });
-  //     let mailOptions = {
-  //       from: "noreply@socioprophet.ai",
-  //       to: email,
-  //       subject: "SignIn Requested",
-  //       html: `
-  //         <h1>SocioProphet&#174;</h1>
-  //         <p>Hello,</p>
-  //         <p>We received a request from this email to sign in to SocioProphet.</p>
-  //         <p>If you would like to sign in via ${email}, you can follow this link:</p>
-  //         <a href=${link}>Sign in to SocioProphet!</a>
-  //         <br />
-  //         <p>If you did not request this link to sign in to SocioProphet, you can safely ignore this email.</p>
-  //         <br />
-  //         <p>Thanks!</p>
-  //         <p>The SocioProphet Team</p>
-  //       `,
-  //     };
-  //     transporter.sendMail(mailOptions, function (err, data) {
-  //       if (err) {
-  //         console.log("Error " + err);
-  //       } else {
-  //         console.log("Email sent successfully");
-  //       }
-  //     });
-  //   })
-  //   .catch((err) => {
-  //     console.error("An error happened: " + err);
-  //   });
-  res.json({
-    "gmail-username": process.env.GMAIL_USERNAME,
-    "gmail-password": process.env.GMAIL_PASSWORD,
-    "gmail-client-id": process.env.GMAIL_CLIENT_ID,
-    "gmail-client-secret": process.env.GMAIL_CLIENT_SECRET,
-    "gmail-refresh-token": process.env.GMAIL_REFRESH_TOKEN,
-  });
+router.post("/data", (req, res) => {
+  const email = req.body.email;
+  const actionCodeSettings = {
+    url: "http://localhost:8081/signup",
+    handleCodeInApp: true,
+  };
+  admin
+    .auth()
+    .generateSignInWithEmailLink(email, actionCodeSettings)
+    .then((link) => {
+      let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.GMAIL_USERNAME,
+          pass: process.env.GMAIL_PASSWORD,
+          clientId: process.env.GMAIL_CLIENT_ID,
+          clientSecret: process.env.GMAIL_CLIENT_SECRET,
+          refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+        },
+      });
+      let mailOptions = {
+        from: "noreply@socioprophet.ai",
+        to: email,
+        subject: "SignIn Requested",
+        html: `
+          <h1>SocioProphet&#174;</h1>
+          <p>Hello,</p>
+          <p>We received a request from this email to sign in to SocioProphet.</p>
+          <p>If you would like to sign in via ${email}, you can follow this link:</p>
+          <a href=${link}>Sign in to SocioProphet!</a>
+          <br />
+          <p>If you did not request this link to sign in to SocioProphet, you can safely ignore this email.</p>
+          <br />
+          <p>Thanks!</p>
+          <p>The SocioProphet Team</p>
+        `,
+      };
+      transporter.sendMail(mailOptions, function (err, data) {
+        if (err) {
+          console.log("Error " + err);
+        } else {
+          console.log("Email sent successfully");
+        }
+      });
+    })
+    .catch((err) => {
+      console.error("An error happened: " + err);
+    });
+  // res.json({
+  //   "gmail-username": process.env.GMAIL_USERNAME,
+  //   "gmail-password": process.env.GMAIL_PASSWORD,
+  //   "gmail-client-id": process.env.GMAIL_CLIENT_ID,
+  //   "gmail-client-secret": process.env.GMAIL_CLIENT_SECRET,
+  //   "gmail-refresh-token": process.env.GMAIL_REFRESH_TOKEN,
+  // });
+  res.send("gg");
 });
 
 router.get("/rss", (req, res) => {
