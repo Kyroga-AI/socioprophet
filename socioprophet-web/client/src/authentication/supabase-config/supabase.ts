46be2704f2cc406d2ba82f63e1d89239f63daccb
Will Jones <willjones484@gmail.com>
2021-10-06 10:46:58 +1100

    aliased edit

--- socioprophet-web/client/src/authentication/supabase-config/supabase.ts
+++ socioprophet-web/client/src/authentication/supabase-config/supabase.ts
@@ -1,15 +1,15 @@
-// import { createClient } from '@supabase/supabase-js';
-
-// const supabaseUrl = 'https://qvxokradfbdvxjlgtues.supabase.co';
-// const supabaseAnonKey =
-//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTk1NjgxNSwiZXhwIjoxOTQ1NTMyODE1fQ.Lcygr9azVIGV2wgSjLXbQNukrw89Gg4sTNEsxz3HfYA';
-
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'http://137.184.132.251:8000';
-const supabaseKey =
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTYyNzIwODU0MCwiZXhwIjoxOTc0MzYzNzQwLCJhdWQiOiIiLCJzdWIiOiIiLCJyb2xlIjoiYW5vbiJ9.sUHErUOiKZ3nHQIxy-7jND6B80Uzf9G4NtMLmL6HXPQ';
+const supabaseUrl = 'https://qvxokradfbdvxjlgtues.supabase.co';
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTk1NjgxNSwiZXhwIjoxOTQ1NTMyODE1fQ.Lcygr9azVIGV2wgSjLXbQNukrw89Gg4sTNEsxz3HfYA';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+// import { createClient } from '@supabase/supabase-js';
+
+// const supabaseUrl = 'http://137.184.132.251:8000';
+// const supabaseKey =
+//   'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTYyNzIwODU0MCwiZXhwIjoxOTc0MzYzNzQwLCJhdWQiOiIiLCJzdWIiOiIiLCJyb2xlIjoiYW5vbiJ9.sUHErUOiKZ3nHQIxy-7jND6B80Uzf9G4NtMLmL6HXPQ';
+
+// export const supabase = createClient(supabaseUrl, supabaseKey);
