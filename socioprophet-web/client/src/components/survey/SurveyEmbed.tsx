befc234fcb161eb1c94adb582663418fc80a0532
William Jones <will@socioprophet.ai>
2023-07-09 12:07:26 +0000

    Chore/refactor

--- socioprophet-web/client/src/components/survey/SurveyEmbed.tsx
+++ socioprophet-web/client/src/components/survey/SurveyEmbed.tsx
@@ -20,7 +20,6 @@ const SurveyEmbed = (id: { id: string }): JSX.Element => {
         id="typeform-full"
         width="100%"
         height="100%"
-        frameBorder="0"
         allow="camera; microphone; autoplay; encrypted-media;"
         src={getSrc()}
       ></iframe>
