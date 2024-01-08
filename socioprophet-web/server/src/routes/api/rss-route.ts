79e805ead50b932d0f7aa1c5e7e1c4ede118928c
Will Jones <willjones484@gmail.com>
2024-01-08 21:41:42 +1100

    add typescript to server

--- socioprophet-web/server/src/routes/api/rss-route.ts
+++ socioprophet-web/server/src/routes/api/rss-route.ts
@@ -1,30 +1,41 @@
+export {};
 const express = require("express");
 const { JSDOM } = require("jsdom");
 const NodeCache = require("node-cache");
-const fetch = require("node-fetch");
-const router = express.Router();
 const { HN_URL } = require("../../constants/index");
+
+const { Response: Res } = express;
+
+const router = express.Router();
 
 const cache = new NodeCache({ stdTTL: 600 });
 
-const fetchHackerNewsFeed = async (res) => {
+const fetchHackerNewsFeed = async (res: typeof Res) => {
   const feedResponse = await fetch(HN_URL);
   const feedData = await feedResponse.text();
 
   const dom = new JSDOM(feedData, { contentType: "text/xml" });
 
-  const items = Array.from(dom.window.document.querySelectorAll("item")).map(
-    (item) => ({
-      title: item.querySelector("title").textContent,
-      link: item.querySelector("link").textContent,
-    })
-  );
+  type FeedItem = {
+    title: string;
+    link: string;
+  };
+
+  const items: FeedItem[] = Array.from(
+    dom.window.document.querySelectorAll("item")
+  ).map((itemElement) => {
+    const item = itemElement as Element;
+    const title = item.querySelector("title")?.textContent || "";
+    const link = item.querySelector("link")?.textContent || "";
+
+    return { title, link };
+  });
 
   cache.set(HN_URL, items);
   res.send(items);
 };
 
-router.get("/rss", (_req, res) => {
+router.get("/rss", (_req: Request, res: typeof Res) => {
   const cachedData = cache.get(HN_URL);
 
   if (cachedData) {
