dad4aa56e63546adfd03d1199a70d4b39f5d4df8
Michael Heller <michaelheller@Michaels-MacBook-Air.local>
2026-02-10 22:10:06 -0500

    build(web): load .env.local/.env.sample and inject FIREBASE_* into webpack bundle

--- socioprophet-web/client/webpack.config.js
+++ socioprophet-web/client/webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
@@ -56,6 +57,17 @@ const webpackConfig = () => {
       },
     },
     plugins: [
+new (require('webpack')).DefinePlugin({
+  'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY || ''),
+  'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN || ''),
+  'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID || ''),
+  'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET || ''),
+  'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID || ''),
+  'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID || ''),
+  'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID || ''),
+  'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL || ''),
+}),
+
       new HtmlWebpackPlugin({
         template: './public/index.html',
         favicon: './public/favicon.ico',
